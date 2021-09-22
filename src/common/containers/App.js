681876d2f82cf4275f5bcb7855e4b0c97cb20794
Andrew <andrew@bbm.co.uk>
2021-09-22 15:58:12 +0100

    Add Class to HTML tag

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,9 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext("light");
 
 export default function App({ children }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark-mode", isDarkMode);
+  }, [isDarkMode]);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
